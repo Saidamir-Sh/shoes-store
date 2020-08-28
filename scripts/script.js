af34db892b0a9755de532cbc6d9e7d381be75392
Saidamir Sharofuddinov <69676883+Saidamir-Sh@users.noreply.github.com>
2020-08-28 21:53:40 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+// jQuery effects 
+
+$('.shoes').waypoint(function(direction) {
+    $('.shoes').addClass('animate__pulse');
+}, {
+    offset: '100%'
+});
+
+$('.card').waypoint(function(direction) {
+    $('.card').addClass('animate__flipInX');
+}, {
+    offset: '100%'
+});
+$('.motive').waypoint(function(direction) {
+    $('.motive').addClass('animate__fadeInUp');
+}, {
+    offset: '100%'
+});
+$('.about-nike').waypoint(function(direction) {
+    $('.about-nike').addClass('animate__fadeInUp');
+}, {
+    offset: '100%'
+});
+
+$('.map').waypoint(function(direction) {
+    $('.map').addClass('animate__fadeIn');
+}, {
+    offset: '100%'
+});
+
+$('.contact').waypoint(function(direction) {
+    $('.contactgi').addClass('animate__fadeInUp');
+}, {
+    offset: '100%'
+});
+
